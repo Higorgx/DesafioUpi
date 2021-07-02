cd2336439279b510e38f5995489dfc0c098fdca1
higorgx <higorg_freitas@hotmail.com>
2021-07-02 15:31:04 -0300

    book imports

--- server/app.py
+++ server/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 
-
 # configuration
 DEBUG = True
 
@@ -18,6 +17,32 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 def ping_pong():
     return jsonify('pong!')
 
+@app.route('/books', methods=['GET'])
+def all_books():
+    return jsonify({
+        'status': 'success',
+        'books': BOOKS
+    })
+
+
+BOOKS = [
+    {
+        'title': 'On the Road',
+        'author': 'Jack Kerouac',
+        'read': True
+    },
+    {
+        'title': 'Harry Potter and the Philosopher\'s Stone',
+        'author': 'J. K. Rowling',
+        'read': False
+    },
+    {
+        'title': 'Green Eggs and Ham',
+        'author': 'Dr. Seuss',
+        'read': True
+    }
+]
 
 if __name__ == '__main__':
     app.run()
+    
